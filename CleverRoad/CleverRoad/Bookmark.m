35d9b0857c55ba7f0cfd9dacd802f38cb7915b14
Leonid Kokhnovych <leonspeedon@gmail.com>
2015-01-25 16:04:48 +0300

    Create initial main view UI, setup Core Data model with its managed object sub-class

--- CleverRoad/CleverRoad/Bookmark.m
+++ CleverRoad/CleverRoad/Bookmark.m
@@ -1,15 +1,17 @@
 //
-//  ViewController.h
+//  Bookmark.m
 //  CleverRoad
 //
 //  Created by Admin on 1/25/15.
 //  Copyright (c) 2015 leonidkokhnovych. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "Bookmark.h"
 
-@interface ViewController : UIViewController
 
+@implementation Bookmark
+
+@dynamic name;
+@dynamic location;
 
 @end
-
---
+++ CleverRoad/CleverRoad/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  CleverRoad
+//
+//  Created by Admin on 1/25/15.
+//  Copyright (c) 2015 leonidkokhnovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
