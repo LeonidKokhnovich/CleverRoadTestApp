3190fa01ca564cc59fb5008877b56627ee0a4808
Leonid Kokhnovych <leonspeedon@gmail.com>
2015-01-25 15:28:01 +0300

    Create project

---
+++ CleverRoad/CleverRoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CleverRoad
+//
+//  Created by Admin on 1/25/15.
+//  Copyright (c) 2015 leonidkokhnovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CleverRoad/CleverRoad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CleverRoad
+//
+//  Created by Admin on 1/25/15.
+//  Copyright (c) 2015 leonidkokhnovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
